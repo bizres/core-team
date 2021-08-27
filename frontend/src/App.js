974d999561b70c354617b7e20d33781c02306f16
Cahit Atilgan <cahit@atilgan.ch>
2021-08-27 22:22:27 +0200

    Project setup (based on awesome-docker) with React, Express and MySQL

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import logo from './prototype_fund.jpeg'
+import './App.css'
+
+function App () {
+  const [message, setMessage] = useState()
+  useEffect(() => {
+    fetch('/api/')
+      .then(res => res.json())
+      .then(res => setMessage(res.message))
+      .catch(console.error)
+  }, [setMessage])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <p>
+          businessresponsibility.ch
+        </p>
+        <p>{message || 'Loading...'}</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
